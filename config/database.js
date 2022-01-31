c53400ea5b5cc60ad86dd33001ad1d3906672236
dan-mutua <daniel.mutua@student.moringaschool.com>
2022-01-31 12:13:28 +0300

    add crud

--- config/database.js
+++ config/database.js
@@ -9,3 +9,22 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'mutua'),
+//       password: env('DATABASE_PASSWORD', 'mutua'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
